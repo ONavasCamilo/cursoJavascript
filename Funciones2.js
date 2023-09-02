66bc4d7d7f55f15d82e3312d27f412c563e68df2
CaNavas <navasochoa@hotmail.com>
2023-09-01 19:00:27 -0500

    segundo commit curso

--- Funciones2.js
+++ Funciones2.js
@@ -2,3 +2,21 @@ function cuidadoConElConsoleLog(nombre) {
     console.log(nombre);
     return nombre;
 }
+
+
+function otraFuncion() {
+    return ("el nombre retornado por la funcion 'cuidadoConElConsoleLog' es: " + 
+    cuidadoConElConsoleLog('Camilo')
+    ); 
+}
+
+function cuidadoConElReturn(nombre) {
+    return nombre
+    console.log(nombre);
+}
+
+
+function pruebaParaComprobarCommit(commit) {
+    return commit
+    console.log(nombre);
+}
